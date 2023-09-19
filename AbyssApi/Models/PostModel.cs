cccf7958b6eac236016255a71be2b3d73aaa020c
Gasshette <gesshokun@gmail.com>
2023-09-19 17:15:50 +0200

    fix (.NET version): switch to .NET 6
    Switch to .NET 6 to install EF core properly

---
+++ AbyssApi/Models/PostModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AbyssApi.Models
+{
+    public class PostModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string imageUrl { get; set; } 
+    }
+}
